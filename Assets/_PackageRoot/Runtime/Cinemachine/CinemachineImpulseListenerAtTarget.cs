0a1fdd607ec460544fda4fe2a0161ac15d3ff161
Émeric Coudé <emericmax9@hotmail.com>
2024-05-20 15:19:51 -0400

    major: upgrading to unity 6

--- Assets/_PackageRoot/Runtime/Cinemachine/CinemachineImpulseListenerAtTarget.cs
+++ Assets/_PackageRoot/Runtime/Cinemachine/CinemachineImpulseListenerAtTarget.cs
@@ -1,12 +1,58 @@
 using UnityEngine;
 
 #if CINEMACHINE
-using Cinemachine;
+using Unity.Cinemachine;
 #endif
 
 namespace Emericoude.Cinemachine
 {
-#if CINEMACHINE
+#if CINEMACHINE_3
+	/// <summary> Overrides the <seealso cref="CinemachineImpulseListener"/> to listen for impulses from the target's location. </summary>
+	/// <remarks> See related thread <see href="https://forum.unity.com/threads/cinemachine-impulse-listener-on-player-object.539063/"></see> </remarks>
+	[SaveDuringPlay]
+	[AddComponentMenu("")] // Hide in menu
+	[ExecuteAlways]
+	public class CinemachineImpulseListenerAtTarget : CinemachineImpulseListener
+	{
+		/// <summary> The target from which to listen for impulses. </summary>
+		[SerializeField] private Transform target;
+
+		/// <summary> React to any detected impulses. </summary>
+		/// <param name="vcam"> The virtual camera being processed. </param>
+		/// <param name="stage"> The current pipeline stage. </param>
+		/// <param name="state"> The current virtual camera state. </param>
+		/// <param name="deltaTime"> The current applicable deltaTime. </param>
+		/// <remarks> This is an override of <see cref="CinemachineImpulseListener"/> which modifies <see cref="CinemachineImpulseManager.GetImpulseAt(Vector3, bool, int, out Vector3, out Quaternion)"/> to use the <see cref="target"/>'s position. </remarks>
+		protected override void PostPipelineStageCallback (CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+		{
+			if (stage != ApplyAfter || !(deltaTime >= 0)) return;
+			
+			//this is copied from the base class, the only difference is target.position inside of GetImpulseAt.
+			bool hasImpulse = CinemachineImpulseManager.Instance.GetImpulseAt(target.position, Use2DDistance, ChannelMask, out var impulsePos, out var impulseRot);
+			bool hasReaction = ReactionSettings.GetReaction(deltaTime, impulsePos, out var reactionPos, out var reactionRot);
+
+			if (hasImpulse)
+			{
+				impulseRot = Quaternion.SlerpUnclamped(Quaternion.identity, impulseRot, Gain);
+				impulsePos *= Gain;
+			}
+
+			if (hasReaction)
+			{
+				impulsePos += reactionPos;
+				impulseRot *= reactionRot;
+			}
+
+			if (hasImpulse || hasReaction)
+			{
+				if (UseCameraSpace) impulsePos = state.RawOrientation * impulsePos;
+
+				state.PositionCorrection += impulsePos;
+				state.OrientationCorrection = state.OrientationCorrection * impulseRot;
+			}
+		}
+	}
+#elif CINEMACHINE_2
 	/// <summary> Overrides the <seealso cref="CinemachineImpulseListener"/> to listen for impulses from the target's location. </summary>
 	/// <remarks> See related thread <see href="https://forum.unity.com/threads/cinemachine-impulse-listener-on-player-object.539063/"></see> </remarks>
 	[SaveDuringPlay]
@@ -52,5 +98,7 @@ namespace Emericoude.Cinemachine
 			}
 		}
 	}
+#else
+public class CinemachineImpulseListenerAtTarget : Monobehaviour { }
 #endif
 }
