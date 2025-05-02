d57116fcd7e3574306c127de321e4ab8f632f367
Émeric Coudé <71528771+emericoude@users.noreply.github.com>
2025-05-02 19:16:59 -0400

    Added a standalone state machine component that auto updates while active

---
+++ Assets/com.emericoude.tools/Runtime/StateMachine/StandaloneStateMachineMonoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine.Serialization;
+
+namespace Emericoude.StateMachine
+{
+    public class StandaloneStateMachineMonoBehaviour : StateMachineMonoBehaviour
+    {
+        public bool autoEnableAndInitOnStart;
+
+        protected virtual void Start() {
+            if (this.autoEnableAndInitOnStart) {
+                this.SetActiveAndEnabled(true);
+                this.OnInit();
+                this.OnEnter();
+            }
+        }
+
+        protected virtual void Update() {
+            if (this.IsActive) this.OnUpdate();
+        }
+
+        protected virtual void FixedUpdate() {
+            if (this.IsActive) this.OnFixedUpdate();
+        }
+
+        protected void OnDisable() {
+            if (this.IsActive) this.OnExit();
+        }
+
+        protected virtual void OnDestroy() {
+            if (this.IsActive) this.OnExit();
+        }
+    }
+}
