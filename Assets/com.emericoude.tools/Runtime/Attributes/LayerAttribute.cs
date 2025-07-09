345ed08c70c4711c3749345f53964abfcb1feea5
Émeric Coudé <71528771+emericoude@users.noreply.github.com>
2025-07-09 16:40:51 -0400

    Moved attributes editor drawers to their own folders

--- Assets/com.emericoude.tools/Runtime/Attributes/LayerAttribute.cs
+++ Assets/com.emericoude.tools/Runtime/Attributes/LayerAttribute.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 namespace Emericoude
 {
     /// <summary> Attribute to select a single layer. Apply to an int field. </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class LayerAttribute : PropertyAttribute {
         
     }
