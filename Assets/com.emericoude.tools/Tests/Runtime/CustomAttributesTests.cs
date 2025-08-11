11cc956a7ae3631fca621e40109a1cc5228845fe
Émeric Coudé <71528771+emericoude@users.noreply.github.com>
2025-08-11 18:08:21 -0400

    Moved some scripts around

--- Assets/com.emericoude.tools/Tests/Runtime/CustomAttributesTests.cs
+++ Assets/com.emericoude.tools/Tests/Runtime/CustomAttributesTests.cs
@@ -1,15 +1,25 @@
 using System;
 
+using Emericoude.Attributes;
+
 using UnityEngine;
 
 namespace Emericoude.Tests
 {
     public class CustomAttributesTests : MonoBehaviour
     {
+        [BetterCurveField]
+        [SerializeField] private AnimationCurve curve;
+        
         [Layer]
         [SerializeField] private int layer;
 
         [RadiansDisplayedAsDegrees]
         [SerializeField] private float angle;
+
+        [DrawInDebugInfoBox]
+        [SerializeField] private float myFloat;
+        [DrawInDebugInfoBox]
+        [SerializeField] private string myString;
     }
 }
